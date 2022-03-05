eb96d3401337a185668bb3724118ba4729148099
Konstantin Fomenkov <kolij2005@gmail.com>
2022-03-05 20:32:53 +0300

    outlet action

--- Color picker/ViewController.swift
+++ Color picker/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  Color picker
-//
-//  Created by Konstantin Fomenkov on 05.03.2022.
-//
+    //
+    //  ViewController.swift
+    //  Color picker
+    //
+    //  Created by Konstantin Fomenkov on 05.03.2022.
+    //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+    }
+    @IBAction func redSliderChanged() {
+
     }
 
+    @IBAction func greenSliderChanged() {
+
+    }
+
+    @IBAction func blueSliderChanged() {
+    }
 
 }
 
